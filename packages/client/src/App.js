3cf29532934e4c1fc5f9c74aabfdb0aebd01123b
iamgbayer <guiilherme.bayer@gmail.com>
2020-06-20 09:53:24 -0300

    Initial commit

---
+++ packages/client/src/App.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef } from "react";
+import * as Y from "yjs";
+import { WebsocketProvider } from "y-websocket";
+import {
+  ySyncPlugin,
+  yCursorPlugin,
+  yUndoPlugin,
+  undo,
+  redo,
+} from "y-prosemirror";
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import { exampleSetup } from "prosemirror-example-setup";
+import { keymap } from "prosemirror-keymap";
+import randomColor from "randomcolor";
+
+import "./prosemirror.css";
+
+import { schema } from "./schema";
+
+function App() {
+  const view = useRef();
+  const container = useRef();
+
+  useEffect(() => {
+    const ydoc = new Y.Doc();
+    const type = ydoc.getXmlFragment("prosemirror");
+
+    const provider = new WebsocketProvider(
+      "ws://localhost:1234",
+      "prosemirror",
+      ydoc
+    );
+
+    provider.awareness.setLocalStateField("user", {
+      name: `Guilherme ${Math.random().toString(36).substring(7)}`,
+      color: randomColor(),
+    });
+
+    view.current = new EditorView(container.current, {
+      state: EditorState.create({
+        schema,
+        plugins: [
+          ySyncPlugin(type),
+          yCursorPlugin(provider.awareness),
+          yUndoPlugin(),
+          keymap({
+            "Mod-z": undo,
+            "Mod-y": redo,
+            "Mod-Shift-z": redo,
+          }),
+        ].concat(exampleSetup({ schema })),
+      }),
+    });
+
+    return () => view.current.destroy();
+  }, []);
+
+  return <div ref={container}></div>;
+}
+
+export default App;
